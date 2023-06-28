76a01fcad4022a46579645cbc171df464103ef99
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-28 19:35:22 -0300

    Apply use state to car widget

--- casmart-ecommerce/src/components/NavBar/CartWidget/CartWidget.js
+++ casmart-ecommerce/src/components/NavBar/CartWidget/CartWidget.js
@@ -1,11 +1,14 @@
+import {useState} from "react";
 import './CartWidget.css';
 
 function CartWidget() {
+    const [counter, setCounter] = useState(4);
+
     return (
         <button className="header-action-btn">
             <ion-icon name="bag-handle-outline" aria-hidden="true"></ion-icon>
             <p className="header-action-label">Cart</p>
-            <div className="btn-badge green" aria-hidden="true">3</div>
+            <div className="btn-badge green" aria-hidden="true">{counter}</div>
         </button>
     );
 }
